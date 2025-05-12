065652a1ad12f6e77685c469ca86820a2a892abf
davarng <113583854+davarng@users.noreply.github.com>
2025-05-12 16:16:27 +0200

    Service

---
+++ BandsApp.Web/Services/BandService.cs
@@ -0,0 +1,18 @@
+﻿namespace BandsApp.Web.Services
+{
+    public class BandService
+    {
+        List<Band> Bands =
+    [
+        new Company { Id = 1, Name = "Company A", Description = "awdadw" },
+        new Company { Id = 2, Name = "Company B" Description = "nawdaw" },
+        new Company { Id = 3, Name = "Company C" Description = "efsef" }
+    ];
+
+        public Band[] GetAll() => [.. bands.OrderBy(o => o.Name)];
+
+
+        public Band? GetById(int id) => bands
+           .SingleOrDefault(o => o.Id == id);
+    }
+}
