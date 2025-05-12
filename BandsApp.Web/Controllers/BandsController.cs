d032acbc2d8e190a7093b6aafa6e4bbabf3f47eb
davarng <113583854+davarng@users.noreply.github.com>
2025-05-12 17:05:57 +0200

    Finished details page

--- BandsApp.Web/Controllers/BandsController.cs
+++ BandsApp.Web/Controllers/BandsController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BandsApp.Web.Controllers
 {
-   
+
     public class BandsController : Controller
     {
         BandService bandService = new BandService();
@@ -12,6 +12,14 @@ namespace BandsApp.Web.Controllers
         public IActionResult Index()
         {
             var model = bandService.GetAllBands();
+            return View(model);
+        }
+
+
+        [Route("details/{id}")]
+        public IActionResult Details(int id)
+        {
+            var model = bandService.GetBandById(id);
             return View(model);
         }
     }
