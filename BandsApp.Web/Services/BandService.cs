eb21c64d98f0abfc858c7e7f1cb3ed0d9ce457be
albertito1985 <anulo@live.se>
2025-05-13 10:30:25 +0200

    Video aresses added

--- BandsApp.Web/Services/BandService.cs
+++ BandsApp.Web/Services/BandService.cs
@@ -6,9 +6,9 @@ namespace BandsApp.Web.Services
     {
         List<Band> bands =
     [
-        new Band { Id = 1, Name = "Queen", Description = "Queen is a band." },
-        new Band { Id = 2, Name = "Beatles", Description = "Beatles is a band." },
-        new Band { Id = 3, Name = "Aerosmith", Description = "Aerosmith is a band." }
+        new Band { Id = 1, Name = "Queen", Description = "Queen is a band.", VideoAddress = "https://www.youtube.com/embed/fJ9rUzIMcZQ?si=hFNKb_FsrMLoIxE1" },
+        new Band { Id = 2, Name = "Beatles", Description = "Beatles is a band.", VideoAddress = "https://www.youtube.com/embed/CGj85pVzRJs?si=6ltuaQgdZMgYmHRF" },
+        new Band { Id = 3, Name = "Aerosmith", Description = "Aerosmith is a band.", VideoAddress = "https://www.youtube.com/embed/NMNgbISmF4I?si=_NNpviQ3pudL7PgY" }
     ];
 
         public Band[] GetAllBands() => [.. bands.OrderBy(o => o.Name)];
